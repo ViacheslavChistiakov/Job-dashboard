6fd88dfed948c12d4ce1248126cb6660d4821a7f
Vyacheslav <chystiakov.vycheslav@gmail.com>
2026-06-04 17:12:13 +0300

    pagination and infinity scroll has been added

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,42 @@
 <template>
-    <div>
-        <h1 class="text-2xl font-bold mb-4">Job Listings</h1>
-        <div v-for="job in jobs" :key="job.id">
-            <JobCard :job="job" />
-        </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Job Listings</h1>
+    <div v-for="job in jobs" :key="job.id">
+      <JobCard :job="job" />
     </div>
+    <div v-if="error" class="text-red-500">
+      {{ error }}
+    </div>
+    <div ref="trigger" class="h-10"></div>
+  </div>
 </template>
 
-<script setup lang="ts">    
+<script setup lang="ts">
 import { getJobs } from '@/server/api/jobs.get';
 import JobCard from '@/components/job/JobCard.vue';
-const { jobs, loading, error, fetchJobs } = getJobs();
-onMounted(() => {
-    fetchJobs();
+const { jobs, loading, error, hasMore, fetchJobs } = getJobs();
+const trigger = ref<HTMLDivElement | null>(null);
+let isFetching = false;
+onMounted(async () => {
+  await fetchJobs();
+  await nextTick();
+  const observer = new IntersectionObserver(
+    async (entries) => {
+      const entry = entries[0] as IntersectionObserverEntry;
+      if (entry.isIntersecting && !loading.value && hasMore.value) {
+        isFetching = true;
+        await fetchJobs();
+        isFetching = false;
+      }
+    },
+    {
+      root: null,
+      rootMargin: '200px',
+      threshold: 0,
+    },
+  );
+  if (trigger.value) {
+    observer.observe(trigger.value);
+  }
 });
 </script>
--- app/server/api/jobs.get.ts
+++ app/server/api/jobs.get.ts
@@ -1,34 +1,45 @@
-import axios from "axios";
-import type { Job } from "~/types/job";
+import axios from 'axios';
+import type { Job } from '~/types/job';
 
-export const getJobs =  () => {
-    const jobs = ref<Job[]>([]);
-    const loading = ref<boolean>(false);
-    const error = ref<null | unknown>(null);
+export const getJobs = () => {
+  const page = ref<number>(1);
+  const pageSize = ref<number>(5);
+  const hasMore = ref<boolean>(true);
+  const jobs = ref<Job[]>([]);
+  const loading = ref<boolean>(false);
+  const error = ref<null | unknown>(null);
 
-    const fetchJobs = async () => {
-        loading.value = true;
-        error.value = null;
-        try {
-            const { data } = await axios.get<Job[]>('https://6a20792be96c1d13b5877d0e.mockapi.io/api/v1/jobs');
-            jobs.value = data.slice(0, 10); 
-        } catch (err) {
-           if (axios.isAxiosError(err)) {
-                error.value = err.response?.data || err.message;
-            } else if (err instanceof Error) {
-                error.value = err.message;
-            } else {
-                error.value = 'An unknown error';
-            }
-        } finally {
-            loading.value = false;
-        }
+  const fetchJobs = async () => {
+    loading.value = true;
+    error.value = null;
+    try {
+      const { data } = await axios.get<Job[]>(
+        `https://6a20792be96c1d13b5877d0e.mockapi.io/api/v1/jobs?page=${page.value}&limit=${pageSize.value}`,
+      );
+      const jobsData = data || [];
+      if (jobsData.length < pageSize.value) {
+        hasMore.value = false;
+      }
+      jobs.value.push(...jobsData);
+      page.value++;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        error.value = err.response?.data || err.message;
+      } else if (err instanceof Error) {
+        error.value = err.message;
+      } else {
+        error.value = 'An unknown error';
+      }
+    } finally {
+      loading.value = false;
     }
+  };
 
-    return {
-        jobs,
-        loading,
-        error,
-        fetchJobs
-    };
-}
+  return {
+    jobs,
+    loading,
+    hasMore,
+    error,
+    fetchJobs,
+  };
+};
